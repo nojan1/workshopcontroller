a17a71a37cef97d92099c2cbf23480eda8bce8d3
Niklas Hedlund <nojan1989@gmail.com>
2017-02-23 21:03:51 +0100

    Added heatcontroller service

--- Client/Extra/update.sh
+++ Client/Extra/update.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 cp ../config.py ../config.py.backup
+cp heatcontroller.service heatcontroller.service.backup
 
 git fetch --all
 git reset --hard origin/master
 
 mv ../config.py.backup ../config.py
+mv heatcontroller.service.backup heatcontroller.service
 
 chmod +x ../submit_temperatures.py
 chmod +x update.sh
 chmod +x startup.sh
 
-[ ! -f "/etc/cron.hourly/10submit_temp" ] && ln -s "$(pwd)/../submit_temperatures.py" /etc/cron.hourly/10submit_temp
-[ ! -f "/etc/cron.hourly/11update" ] && ln -s "$(pwd)/update.sh" /etc/cron.hourly/11update
+rm "/etc/cron.hourly/10submit_temp" && ln -s "$(pwd)/../submit_temperatures.py" /etc/cron.hourly/10submit_temp
+rm "/etc/cron.hourly/11update" && ln -s "$(pwd)/update.sh" /etc/cron.hourly/11update
+rm "/etc/systemd/system/heatcontroller.service" && ln -s "$(pwd)/heatcontroller.service" /etc/systemd/system/heatcontroller.service
 
 if [ "$1" != "-norestart" ]; then
     systemctl restart heatcontroller
