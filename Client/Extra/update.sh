84bc9df4bc1a122443f0d4df8e1f399edae3daa6
Niklas Hedlund <nojan1989@gmail.com>
2017-02-23 20:58:01 +0100

    cwd => pwd

--- Client/Extra/update.sh
+++ Client/Extra/update.sh
@@ -11,8 +11,8 @@ chmod +x ../submit_temperatures.py
 chmod +x update.sh
 chmod +x startup.sh
 
-[ ! -f "/etc/cron.hourly/10submit_temp" ] && ln -s "$(cwd)/../submit_temperatures.py" /etc/cron.hourly/10submit_temp
-[ ! -f "/etc/cron.hourly/11update" ] && ln -s "$(cwd)/update.sh" /etc/cron.hourly/11update
+[ ! -f "/etc/cron.hourly/10submit_temp" ] && ln -s "$(pwd)/../submit_temperatures.py" /etc/cron.hourly/10submit_temp
+[ ! -f "/etc/cron.hourly/11update" ] && ln -s "$(pwd)/update.sh" /etc/cron.hourly/11update
 
 if [ "$1" != "-norestart" ]; then
     systemctl restart heatcontroller
